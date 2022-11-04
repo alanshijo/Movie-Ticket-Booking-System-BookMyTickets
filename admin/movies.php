e2672a6ec0dd61d3c8cc1ba4c33fb0397c83296d
alanshijo <alanshijo2023a@mca.ajce.in>
2022-11-04 23:58:56 +0530

    movie

--- admin/movies.php
+++ admin/movies.php
@@ -98,6 +98,10 @@ include '../db_conn.php';
               <a class="js-arrow" href="movies.php">
                 <i class="fas fa-film"></i>Movies</a>
             </li>
+            <li class="has-sub">
+              <a class="js-arrow" href="theatres.php">
+                <i class="fa fa-building"></i>Theatres</a>
+            </li>
           </ul>
         </nav>
       </div>
@@ -168,7 +172,7 @@ include '../db_conn.php';
                         <?php
                         require '../db_conn.php';
 
-                        $query = "SELECT * FROM tbl_movies";
+                        $query = "SELECT * FROM tbl_movies WHERE del_status='0'";
                         $query_run = mysqli_query($conn, $query);
                         $i = 1;
                         while ($movie = mysqli_fetch_array($query_run)) {
@@ -183,12 +187,8 @@ include '../db_conn.php';
                             <td><?= $movie['movie_runtime'] ?></td>
                             <td><?= $movie['movie_releasedate'] ?></td>
                             <td>
-                              <a href="#">
-                                <i class="fa fa-edit"></i>
-                              </a>
-                              &ensp;
-                              <a href="#trash-o">
-                                <i class="fa fa-trash"></i></a>
+                              <button type="button" value="<?php echo $movie['movie_id']; ?>" class="editMovieBtn fa fa-edit" style="color: #0056b3;"></button> &nbsp;
+                              <button type="button" value="<?php echo $movie['movie_id']; ?>" class="deleteMovieBtn fa fa-trash" style="color: #0056b3;"></button>
                             </td>
                           </tr>
                         <?php
@@ -265,6 +265,67 @@ include '../db_conn.php';
           </div>
         </div>
       </div>
+      <!-- Edit Student Modal -->
+      <div class="modal fade" id="studentEditModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Edit Student</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form id="updateStudent">
+              <div class="modal-body">
+
+                <!-- <div id="errorMessageUpdate" class="alert alert-warning d-none"></div> -->
+
+                <input type="hidden" name="movie_id" id="movie_id">
+
+                <div class="mb-3">
+                  <label for="">Poster image</label>
+                  <input type="file" class="form-control" name="poster" id="poster" accept=".jpg" />
+                </div>
+                <div class="mb-3">
+                  <label for="">Title</label>
+                  <input type="text" name="title" id="title" class="form-control" placeholder="Enter movie name here" />
+                </div>
+                <div class="mb-3">
+                  <label for="">Language</label>
+                  <select name="lang" id="lang" class="form-control">
+                    <option hidden>Select language</option>
+                    <option value="English">English</option>
+                    <option value="Malayalam">Malayalam</option>
+                    <option value="Tamil">Tamil</option>
+                    <option value="Hindi">Hindi</option>
+                    <option value="Telugu">Telugu</option>
+                    <option value="Kannada">Kannada</option>
+                  </select>
+                </div>
+                <div class="mb-3">
+                  <label for="">Certificate</label>
+                  <select name="certificate" id="certificate" class="form-control">
+                    <option hidden>Select certificate type</option>
+                    <option value="U">U certificate</option>
+                    <option value="U/A">U/A certificate</option>
+                    <option value="A">A certificate</option>
+                    <option value="S">S certificate</option>
+                  </select>
+                </div>
+                <div class="mb-3">
+                  <label for="">Total runtime</label>
+                  <input type="text" name="runtime" id="runtime" class="form-control" placeholder="00hr 00min" />
+                </div>
+                <div class="mb-3">
+                  <label for="">Release date</label>
+                  <input type="date" name="release" id="release" class="form-control" placeholder="DD/MM/YYYY" />
+                </div>
+              </div>
+              <div class="modal-footer">
+                <button type="submit" class="btn btn-primary">Update Movie</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
       <!-- END MAIN CONTENT-->
       <!-- END PAGE CONTAINER-->
     </div>
@@ -297,7 +358,6 @@ include '../db_conn.php';
   <script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
   <!-- Main JS-->
   <script src="js/main.js"></script>
-  <!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> -->
   <script>
     $(document).on('submit', '#saveStudent', function(e) {
       e.preventDefault();
@@ -307,7 +367,7 @@ include '../db_conn.php';
 
       $.ajax({
         type: "POST",
-        url: "savemovie.php",
+        url: "save.php",
         data: formData,
         processData: false,
         contentType: false,
@@ -319,6 +379,77 @@ include '../db_conn.php';
       });
 
     });
+
+    $(document).on('click', '.editMovieBtn', function() {
+
+      var movie_id = $(this).val();
+
+      $.ajax({
+        type: "GET",
+        url: "save.php?movie_id=" + movie_id,
+        success: function(response) {
+
+          var res = jQuery.parseJSON(response);
+          if (res.status == 200) {
+
+            $('#movie_id').val(res.data.movie_id);
+            // $('#poster').val(res.data.movie_poster);
+            $('#title').val(res.data.movie_name);
+            $('#lang').val(res.data.movie_lang);
+            $('#certificate').val(res.data.movie_certificate);
+            $('#runtime').val(res.data.movie_runtime);
+            $('#release').val(res.data.movie_releasedate);
+
+            $('#studentEditModal').modal('show');
+          }
+        }
+      });
+
+    });
+
+    $(document).on('submit', '#updateStudent', function(e) {
+      e.preventDefault();
+
+      var formData = new FormData(this);
+      formData.append("update_movie", true);
+
+      $.ajax({
+        type: "POST",
+        url: "save.php",
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function(response) {
+          $('#studentEditModal').modal('hide');
+          $('#updateStudent')[0].reset();
+
+          $('#myTable').load(location.href + " #myTable");
+
+        }
+      });
+
+    });
+
+    $(document).on('click', '.deleteMovieBtn', function (e) {
+            e.preventDefault();
+
+            if(confirm('Are you sure you want to delete this data?'))
+            {
+                var movie_id = $(this).val();
+                $.ajax({
+                    type: "POST",
+                    url: "save.php",
+                    data: {
+                        'delete_movie': true,
+                        'movie_id': movie_id
+                    },
+                    success: function (response) {
+
+                            $('#myTable').load(location.href + " #myTable");
+                    }
+                });
+            }
+        });
   </script>
 
 </body>

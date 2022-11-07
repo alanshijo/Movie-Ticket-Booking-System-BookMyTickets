286afd3e33783972f44d5a436e3a3da3f6a932dc
alanshijo <alanshijo2023a@mca.ajce.in>
2022-11-07 12:15:18 +0530

    db updated

--- database/bookmytickets.sql
+++ database/bookmytickets.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 06, 2022 at 02:51 PM
+-- Generation Time: Nov 07, 2022 at 07:44 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -40,7 +40,7 @@ CREATE TABLE `tbl_login` (
 --
 
 INSERT INTO `tbl_login` (`login_id`, `email`, `password`, `otp_code`, `type_id`) VALUES
-(1, 'admin@admin.com', 'Admin@123', '0', 1),
+(1, 'admin@admin.com', 'qwerty', '0', 1),
 (4, 'alanshijoatkl@gmail.com', 'Alan@1234', '0', 2),
 (8, 'alexreji777@gmail.com', '1234', '0', 2),
 (16, 'georgebenny456@gmail.com', 'George@1234', '', 2),
@@ -69,20 +69,11 @@ CREATE TABLE `tbl_movies` (
 --
 
 INSERT INTO `tbl_movies` (`movie_id`, `movie_poster`, `movie_name`, `movie_lang`, `movie_certificate`, `movie_runtime`, `movie_releasedate`, `del_status`) VALUES
-(18, 'wallpaperflare.com_wallpaper (2).jpg', 'Spiderman', 'English', 'U/A', '2hr 30min', '2022-11-30', 1),
-(19, 'wallpaperflare.com_wallpaper (2).jpg', 'Spider 2', 'English', 'U/A', '3hr 1min', '2022-12-25', 1),
-(20, 'preview.jpg', 'NFS', 'English', 'U/A', '2hr 0min', '2022-12-01', 1),
-(21, 'preview.jpg', 'nfs 2.0', 'English', 'A', '2tgh5gfv', '2022-11-30', 1),
-(22, 'preview.jpg', 'nfs 3', 'English', 'U', '4erdfygh', '2022-11-23', 1),
-(23, 'preview.jpg', 'nfs 3', 'English', 'U/A', '3hr 1min', '2022-11-30', 1),
-(24, 'wallpaperflare.com_wallpaper (2).jpg', 'spider 4', 'English', 'A', '2h', '2022-11-22', 1),
-(25, 'wallpaperflare.com_wallpaper (2).jpg', 'spider 9', 'English', 'A', '4r5t', '2022-11-29', 1),
-(26, 'wallpaperflare.com_wallpaper (2).jpg', 'spider 6', 'English', 'A', '3ed', '2022-11-30', 0),
-(27, 'wallpaperflare.com_wallpaper.jpg', 'spider 4', 'English', 'U', '2ewd', '2022-12-01', 1),
-(28, 'wallpaperflare.com_wallpaper.jpg', 'werty', 'English', 'U/A', 'tergdfv', '2022-12-06', 0),
-(29, 'preview.jpg', 'nfs4', 'English', 'U/A', '4rf', '2022-11-30', 0),
-(30, 'preview.jpg', 'nfs 8', 'English', 'U/A', '4rewfsd', '2022-11-24', 0),
-(31, 'preview.jpg', 'dfx', 'Malayalam', 'U/A', 'fd', '2022-11-29', 0);
+(32, 'cover.jpg', 'Transformer', 'English', 'U/A', '2hr 10min', '2022-11-30', 0),
+(33, 'cover2.jpg', 'Renevant', 'English', 'U/A', '3hr 1min', '2022-11-21', 0),
+(34, 'cover4.jpg', 'Inception', 'English', 'U', '2hr 30min', '2022-11-15', 0),
+(35, 'cover6.jpg', 'Orphan', 'English', 'A', '1hr 22min', '2022-11-25', 0),
+(36, 'project-1.jpg', 'avatar', 'English', 'U', '2hr 5min', '2022-11-22', 0);
 
 -- --------------------------------------------------------
 
@@ -103,7 +94,28 @@ CREATE TABLE `tbl_shows` (
 
 INSERT INTO `tbl_shows` (`show_id`, `show_name`, `show_time`, `del_status`) VALUES
 (1, 'noon', '13:00:00', 1),
-(2, 'morning', '11:00:00', 0);
+(2, 'morning', '11:00:00', 0),
+(3, 'noon', '14:00:00', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_theatremovies`
+--
+
+CREATE TABLE `tbl_theatremovies` (
+  `tm_id` int(11) NOT NULL,
+  `thtr_id` int(11) NOT NULL,
+  `movie_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_theatremovies`
+--
+
+INSERT INTO `tbl_theatremovies` (`tm_id`, `thtr_id`, `movie_id`) VALUES
+(43, 11, 32),
+(44, 11, 34);
 
 -- --------------------------------------------------------
 
@@ -126,7 +138,29 @@ CREATE TABLE `tbl_theatres` (
 INSERT INTO `tbl_theatres` (`thtr_id`, `thtr_name`, `thtr_place`, `ticket_price`, `del_status`) VALUES
 (11, 'Maharani', 'Palai', 100, 0),
 (12, 'Universal', 'Palai', 130, 0),
-(13, 'Surya', 'Erattupetta', 150, 0);
+(13, 'Surya', 'Erattupetta', 150, 0),
+(14, '', '', 0, 1),
+(15, 'ashirvd', 'thodupuzha', 200, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_theatreshows`
+--
+
+CREATE TABLE `tbl_theatreshows` (
+  `ts_id` int(11) NOT NULL,
+  `show_id` int(11) NOT NULL,
+  `tm_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_theatreshows`
+--
+
+INSERT INTO `tbl_theatreshows` (`ts_id`, `show_id`, `tm_id`) VALUES
+(41, 3, 43),
+(42, 3, 44);
 
 -- --------------------------------------------------------
 
@@ -136,6 +170,7 @@ INSERT INTO `tbl_theatres` (`thtr_id`, `thtr_name`, `thtr_place`, `ticket_price`
 
 CREATE TABLE `tbl_users` (
   `user_id` int(11) NOT NULL,
+  `pro_pic` varchar(255) NOT NULL,
   `user_fname` varchar(25) NOT NULL,
   `user_lname` varchar(25) NOT NULL,
   `user_phno` varchar(10) NOT NULL,
@@ -147,13 +182,13 @@ CREATE TABLE `tbl_users` (
 -- Dumping data for table `tbl_users`
 --
 
-INSERT INTO `tbl_users` (`user_id`, `user_fname`, `user_lname`, `user_phno`, `user_status`, `login_id`) VALUES
-(1, 'admin', 'admin', '9999999999', 'active', 1),
-(2, 'alan', 'shijo', '8281187831', 'active', 4),
-(6, 'Alex', 'Reji', '7415236980', 'active', 8),
-(14, 'George', 'Benny', '6238681837', 'deactive', 16),
-(16, 'Nice', 'Shijo', '9446895431', 'active', 18),
-(17, 'Shijo', 'Joseph', '9946351543', 'active', 19);
+INSERT INTO `tbl_users` (`user_id`, `pro_pic`, `user_fname`, `user_lname`, `user_phno`, `user_status`, `login_id`) VALUES
+(1, '0', 'admin', 'admin', '9999999999', 'active', 1),
+(2, 'project-3.jpg', 'alan', 'shijo', '8281187831', 'active', 4),
+(6, '0', 'Alex', 'Reji', '7415236980', 'deactive', 8),
+(14, '0', 'George', 'Benny', '6238681837', 'deactive', 16),
+(16, '0', 'Nice', 'Shijo', '9446895431', 'active', 18),
+(17, '0', 'Shijo', 'Joseph', '9946351543', 'active', 19);
 
 -- --------------------------------------------------------
 
@@ -197,10 +232,22 @@ ALTER TABLE `tbl_shows`
   ADD PRIMARY KEY (`show_id`);
 
 --
+-- Indexes for table `tbl_theatremovies`
+--
+ALTER TABLE `tbl_theatremovies`
+  ADD PRIMARY KEY (`tm_id`);
+
+--
 -- Indexes for table `tbl_theatres`
 --
 ALTER TABLE `tbl_theatres`
   ADD PRIMARY KEY (`thtr_id`);
+
+--
+-- Indexes for table `tbl_theatreshows`
+--
+ALTER TABLE `tbl_theatreshows`
+  ADD PRIMARY KEY (`ts_id`);
 
 --
 -- Indexes for table `tbl_users`
@@ -228,19 +275,31 @@ ALTER TABLE `tbl_login`
 -- AUTO_INCREMENT for table `tbl_movies`
 --
 ALTER TABLE `tbl_movies`
-  MODIFY `movie_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `movie_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT for table `tbl_shows`
 --
 ALTER TABLE `tbl_shows`
-  MODIFY `show_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `show_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tbl_theatremovies`
+--
+ALTER TABLE `tbl_theatremovies`
+  MODIFY `tm_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 
 --
 -- AUTO_INCREMENT for table `tbl_theatres`
 --
 ALTER TABLE `tbl_theatres`
-  MODIFY `thtr_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `thtr_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `tbl_theatreshows`
+--
+ALTER TABLE `tbl_theatreshows`
+  MODIFY `ts_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT for table `tbl_users`
